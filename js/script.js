6e7588e588aac49439f6084a5cfe83cc40fe417c
José Neto <joseneto1500@gmail.com>
2024-04-28 17:23:55 -0300

    feat: :sparkles: add json server

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const priceInput = document.querySelector("#product-price");
 const imageInput = document.querySelector("#product-image");
 const btnSave = document.querySelector(".btn-save");
 const btnReset = document.querySelector(".btn-reset");
+const btnDelete = document.querySelectorAll(".icon-trash");
 const productsList = document.querySelector(".products");
-const deleteProduct = document.querySelector(".icon-trash");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault()
@@ -17,7 +17,7 @@ btnReset.addEventListener("click", () => {
   form.reset()
 })
 
-function addProduct() {
+async function addProduct() {
   const name = nameInput.value;
   const price = priceInput.value;
   const image = imageInput.value;
@@ -28,22 +28,52 @@ function addProduct() {
     image
   }
 
-  productsList.innerHTML += `
-    <div class="product-card">
-      <div>
-        <img class="product-image" src="${image}" alt="storm">
-      </div>
-      <div>
-        <h4 class="product-name">${name}</h4>
-        <p class="product-price">$ ${price}</p>
-        <img class="icon-trash" src="./assets/images/icon-trash.png" alt="${name}">
-      </div>
-    </div>
-  `
+  await addProductInDb(product);
 
   nameInput.value = "";
   priceInput.value = "";
   imageInput.value = "";
-
-  console.log(product)
 }
+async function addProductInDb(product) {
+  try {
+    const response = await fetch("http://localhost:3000/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product)
+    });
+
+    if (!response.ok) {
+      throw new Error('Erro ao adicionar o produto');
+    }
+
+    const data = await response.json();
+    alert.log('Resposta do servidor:', data);
+
+  } catch (error) {
+    alert.error('Erro ao enviar a requisição:', error);
+  }
+}
+
+
+async function addProductInitial () {
+  const products = await fetch("http://localhost:3000/products")
+  const productsData = await products.json()
+
+  productsData.map((product) => {
+    productsList.innerHTML += `
+      <div class="product-card" key="${product.id}">
+        <div>
+          <img class="product-image" src="${product.image}" alt="storm">
+        </div>
+        <div>
+          <h4 class="product-name">${product.name}</h4>
+          <p class="product-price">$ ${product.price}</p>
+          <img class="icon-trash" src="./assets/images/icon-trash.png" alt="${product.productName}">
+        </div>
+      </div>
+    `
+  })
+}
+addProductInitial()
